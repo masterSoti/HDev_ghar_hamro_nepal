88f88540aa33863ad9281f7b06c5b1df9701cf61
ssoti2201 <ssoti2201@bvsd.org>
2014-09-26 23:02:37 -0600

    I got the english date picker working!!! but I still need to figure out the nepali date picker

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 //===========================================================jquery=================================================================
 
-
 //=========================================================javascript===============================================================
-//date javascript	
-function date_english(){
-	var x = new Date();
-	document.getElementById("home_date_english").innerHTML = x.toDateString();
+//date picker
+window.onload = function(){
+	var dt = new Date();
+	document.getElementById("home_date_english").innerHTML = dt.toDateString() + " || ";
 }
-date_english();
