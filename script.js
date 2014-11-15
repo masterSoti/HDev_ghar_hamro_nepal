69ddb3f46d4dd1f515e75eb2465b82d0c46d86b8
ssoti2201 <ssoti2201@bvsd.org>
2014-11-14 21:45:02 -0700

    shit

--- script.js
+++ script.js
@@ -4,5 +4,5 @@
 //date picker
 window.onload = setTimeout(function(){
 	var dt = new Date();
-	document.getElementById("home_date_english").innerHTML = (dt.toDateString() + " || ");
+	document.getElementById("date").innerHTML = (dt.toDateString() + " || ");
 }, 500);
