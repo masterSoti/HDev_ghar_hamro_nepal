b6c2a596917d82e87648b6604a6d58c4596ee06a
Suyog Soti <ssoti2201@bvsd.org>
2014-12-05 21:25:17 -0700

    Unfinished alerts javascripts

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ window.onload = setTimeout(function(){
 	var dt = new Date();
 	document.getElementById("date").innerHTML = (dt.toDateString() + " || ");
 }, 500);
+$(document).ready(function(){
+	$('td').click(function(){
+		alert("Sorry, but this page has not been finished");
+	});
+});
